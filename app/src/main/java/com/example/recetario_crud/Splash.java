04ff172fa6339aacbcc079790fd4ed6147d27fa1
francereal <frankoko1999@gmail.com>
2019-11-18 07:52:54 -0600

    Se agregó la clase Splash del Splash Screen.

---
+++ app/src/main/java/com/example/recetario_crud/Splash.java
@@ -0,0 +1,17 @@
+package com.example.recetario_crud;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
