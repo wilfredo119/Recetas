fd66526720d970d3563c945aa117fd6b83fb6c89
damian778 <Martinezdamian034@gmail.com>
2019-11-17 23:56:24 -0600

    se creo el metodo de guardar

--- app/src/main/java/com/example/recetario_crud/DB.java
+++ app/src/main/java/com/example/recetario_crud/DB.java
@@ -1,6 +1,8 @@
 package com.example.recetario_crud;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -19,5 +21,21 @@ public class DB extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
+    }
+    public String guardar(String nombre, String ingredientes, String categoria) {
+        String mensaje = "";
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues contenedor = new ContentValues();
+        contenedor.put("nombre", nombre);
+        contenedor.put("ingredientes", ingredientes);
+        contenedor.put("categoria", categoria);
+        try {
+            database.insertOrThrow("datos", null, contenedor);
+            mensaje = "Ingresado Correctamente";
+
+        } catch (SQLException e) {
+            mensaje = "Error, al Ingresar";
+        }
+        return mensaje;
     }
 }
--- app/src/main/java/com/example/recetario_crud/PrincipalActivity.java
+++ app/src/main/java/com/example/recetario_crud/PrincipalActivity.java
@@ -3,12 +3,42 @@ package com.example.recetario_crud;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PrincipalActivity extends AppCompatActivity {
+
+
+    EditText Enombre,Eingredientes,Ecategoria;
+    Button guardar,recetario,lv_lista,lista_1 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        Enombre= (EditText)findViewById(R.id.et_nombre);
+        Eingredientes= (EditText)findViewById(R.id.et_ingredientes);
+        Ecategoria= (EditText)findViewById(R.id.et_categoria);
+
+        guardar=(Button)findViewById(R.id.guardar);
+        recetario=(Button)findViewById(R.id.recetario);
+        lv_lista= (Button)findViewById(R.id.lv_lista);
+        lista_1 = (Button)findViewById(R.id.Lista_1);
+
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DB db = new DB(getApplicationContext(),null,null,1);
+                String nombre = Enombre.getText().toString();
+                String ingredientes = Eingredientes.getText().toString();
+                String categoria = Ecategoria.getText().toString();
+
+                String mensaje = db.guardar(nombre, ingredientes, categoria);
+                Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
