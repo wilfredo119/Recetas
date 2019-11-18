b5a25c14d9f5c5d7fb363202ca7598f037ead3ed
francereal <frankoko1999@gmail.com>
2019-11-18 08:28:41 -0600

    Se agrego la clase Main activity 3. -Cesar

---
+++ app/src/main/java/com/example/recetario_crud/Main3Activity.java
@@ -0,0 +1,24 @@
+package com.example.recetario_crud;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main3Activity extends AppCompatActivity {
+
+    /*ListView listaDias;*/
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+
+        /*listaDias = (ListView)findViewById(R.id.listViewId);
+
+        ArrayAdapter<CharSequence> adaptador=ArrayAdapter.createFromResource(this, R.array.arrayDias , android.R.layout.simple_list_item_1);
+
+listaDias.setAdapter(adaptador);*/
+    }
+}
