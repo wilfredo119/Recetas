8644fff0a33b108af286c5a5fe4c2d00275cee9a
damian778 <Martinezdamian034@gmail.com>
2019-11-18 00:15:18 -0600

    se creara el metodo de buscar reg

--- app/src/main/java/com/example/recetario_crud/Main2Activity.java
+++ app/src/main/java/com/example/recetario_crud/Main2Activity.java
@@ -3,12 +3,25 @@ package com.example.recetario_crud;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    EditText Ebuscar;
+    TextView tv_nombre, tv_ingredientes, tv_categoria;
+    Button Bbuscar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        tv_nombre=(TextView)findViewById(R.id.tv_nombre);
+        tv_ingredientes=(TextView)findViewById(R.id.tv_ingredientes);
+        tv_categoria=(TextView)findViewById(R.id.tv_categoria);
+        Ebuscar= (EditText)findViewById(R.id.Ebuscar);
+        Bbuscar = (Button)findViewById(R.id.Bbuscar);
     }
 }
