c96cf106cf912732be36532bafaa41af2c712075
damian778 <Martinezdamian034@gmail.com>
2019-11-18 00:21:32 -0600

    se termino el metodo de buscar

--- app/src/main/java/com/example/recetario_crud/Main2Activity.java
+++ app/src/main/java/com/example/recetario_crud/Main2Activity.java
@@ -3,9 +3,11 @@ package com.example.recetario_crud;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -23,5 +25,23 @@ public class Main2Activity extends AppCompatActivity {
         tv_categoria=(TextView)findViewById(R.id.tv_categoria);
         Ebuscar= (EditText)findViewById(R.id.Ebuscar);
         Bbuscar = (Button)findViewById(R.id.Bbuscar);
+
+
+        Bbuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DB db = new DB(getApplicationContext(),null,null,1);
+                String buscar = Ebuscar.getText().toString();
+                String[]datos;
+                datos = db.buscar_reg(buscar);
+
+                tv_nombre.setText(datos[0]);
+                tv_ingredientes.setText(datos[1]);
+                tv_categoria.setText(datos[2]);
+
+                Toast.makeText(getApplicationContext(), datos[3], Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
