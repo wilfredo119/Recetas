a1b659d3cc97281327cbe6f772150ead631cf872
wilfredo119 <cubiasmontes11@gmail.com>
2019-11-20 10:15:15 -0600

    se creo el activity

---
+++ app/src/main/java/com/example/recetario_crud/Recicler_View.java
@@ -0,0 +1,34 @@
+package com.example.recetario_crud;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Recicler_View extends AppCompatActivity {
+
+    ArrayList<String> ListDatos;
+    RecyclerView recycler;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recicler__view);
+
+      recycler=(RecyclerView)findViewById(R.id.RecyclerId);
+      recycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+
+      ListDatos= new ArrayList<String>();
+
+      for (int i=0;i<=10;i++){
+          ListDatos.add("datos de recetas # "+i+" ");
+        }
+        AdapterDatos adapter = new AdapterDatos(ListDatos);
+      recycler.setAdapter(adapter);
+    }
+
+}
