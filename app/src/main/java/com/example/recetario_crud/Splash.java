25dcf873a4451ee9c492e3b628f69ff4a72d2833
francereal <frankoko1999@gmail.com>
2019-11-21 06:38:55 -0600

    Se actualizo el manifest y la clase splash.

--- app/src/main/java/com/example/recetario_crud/Splash.java
+++ app/src/main/java/com/example/recetario_crud/Splash.java
@@ -10,7 +10,7 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, Principal_Activity.class);
+        Intent intent = new Intent(this,PrincipalActivity.class);
         startActivity(intent);
         finish();
     }
